c7d145ad693092e833f61e5ebfdb08521f294fe9
Conor Sweeney <conorsweeney1@gmail.com>
2021-11-11 05:35:00 +0200

    Event/Error Logging

--- LLHLS Tester/ViewController.swift
+++ LLHLS Tester/ViewController.swift
@@ -16,10 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var urlLabel: UILabel!
     
+    @IBOutlet weak var tableView: UITableView!
+    
     // This is used to save/access the last used URL
     var userDefaults: UserDefaults {
         return UserDefaults.standard
     }
+    
+    var events = [String]()
     
     @IBAction func setURL(_ sender: Any) {
         guard let urlText = urlTextField.text else {
@@ -27,6 +31,7 @@ class ViewController: UIViewController {
         }
         urlLabel.text = urlText
         userDefaults.set(urlText, forKey: urlKey)
+        addActionEvent(action: "Set URL \(urlText)")
         setPlayerItem()
     }
     
@@ -38,6 +43,7 @@ class ViewController: UIViewController {
 
     @IBAction func pausePress(_ sender: Any) {
         player.pause()
+        addActionEvent(action: "Pause")
     }
     
     @IBAction func playPress(_ sender: Any) {
@@ -45,16 +51,19 @@ class ViewController: UIViewController {
             setPlayerItem()
         }
         player.play()
+        addActionEvent(action: "Play")
     }
     
     @IBAction func stopPress(_ sender: Any) {
         // There is no stop functionality so instead I am destroying the asset in the player
         player.replaceCurrentItem(with: nil)
+        addActionEvent(action: "Stop")
     }
     
     @IBAction func livePress(_ sender: Any) {
         // This seeks to the very end of the stream if a user has paused
         player.seek(to: CMTimeMakeWithSeconds(Float64(MAXFLOAT), preferredTimescale: Int32(NSEC_PER_SEC)))
+        addActionEvent(action: "Live")
     }
     
     private func setPlayerItem(){
@@ -64,6 +73,7 @@ class ViewController: UIViewController {
         }
         let playerItem = AVPlayerItem(url: url)
         player.replaceCurrentItem(with: playerItem)
+        addActionEvent(action: "Player Item Set")
     }
     
     override func viewDidLoad() {
@@ -102,7 +112,10 @@ class ViewController: UIViewController {
                 newStatus = .unknown
             }
             if newStatus == .failed {
-                NSLog("Error: \(String(describing: self.player.currentItem?.error?.localizedDescription)), error: \(String(describing: self.player.currentItem?.error))")
+                guard let localizedError = self.player.currentItem?.error?.localizedDescription else {
+                    return
+                }
+                addErrorEvent(error: localizedError)
             }
         }
     }
@@ -112,16 +125,70 @@ class ViewController: UIViewController {
         guard let object = notification.object, let playerItem = object as? AVPlayerItem else {
             return
         }
+        
         guard let errorLog: AVPlayerItemErrorLog = playerItem.errorLog() else {
             return
         }
-        NSLog("Error: \(errorLog)")
+        
+        guard let localizedError = self.player.currentItem?.error?.localizedDescription else {
+            return
+        }
+        addErrorEvent(error: localizedError)
     }
 
     @objc func failedToPlayToEndTime(_ notification: Notification) {
         if let error = notification.userInfo!["AVPlayerItemFailedToPlayToEndTimeErrorKey"] as? Error {
-            print("Error: \(error.localizedDescription), error: \(error)")
+            addErrorEvent(error: error.localizedDescription)
         }
+    }
+    
+    private func addActionEvent(action: String){
+        let actionStr = "Action: \(action)"
+        addEvent(event: actionStr)
+    }
+    
+    private func addErrorEvent(error: String){
+        let errorStr = "Error: \(error)"
+        addEvent(event: errorStr)
+    }
+    
+    private func addEvent(event: String) {
+        // Format the timestamp
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "MM/dd/yy HH:mm:ss"
+        let dateString = formatter.string(from: now)
+        let eventString = "\(dateString) \(event)"
+        print(event)
+        
+        // Dispatch to the main thread to prevent a race case
+        DispatchQueue.main.async{
+            self.events.append(eventString)
+            self.tableView.reloadData()
+        }
+        
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get reusable cell.
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "reuseId") {
+            // Set text of textLabel.
+            // ... Use indexPath.item to get the current row index.
+            if let label = cell.textLabel {
+                label.text = events[indexPath.item]
+            }
+            // Return cell.
+            return cell
+        }
+        // Return empty cell.
+        return UITableViewCell()
     }
 }
 
